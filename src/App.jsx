84f7111b2ee2998a48d2ccfa587896cb8255f790
Jere <jeeretoledo@gmail.com>
2024-03-13 13:27:20 -0300

    e-commerce 2 Jeremias Toledo

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,10 @@ import { NavBar } from './components/NavBar';
 import { ItemListContainer } from './components/ItemListContainer';
 
 function App() {
-
   return (
     <>
       <NavBar /> 
-      <ItemListContainer greeting='PRODUCTS' />
+      <ItemListContainer greeting ='PRODUCTS' />
     </>
   )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,13 @@
 import Container from 'react-bootstrap/Container';
+import PropTypes from 'prop-types';
 
-export const ItemListContainer = ({greeting}) => (
+
+export const ItemListContainer = ({ greeting }) => (
     <Container className='mt-4'>
-    <h2>{greeting}</h2>
+      <h2>{greeting}</h2>
     </Container>
-)
+  );
+  
+  ItemListContainer.propTypes = {
+    greeting: PropTypes.string.isRequired,
+  };
