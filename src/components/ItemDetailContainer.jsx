38c53280a424d77f0686d058b4c6872087103f20
Jere <jeeretoledo@gmail.com>
2024-05-05 20:45:40 -0300

    Workshop-Spinner-Jeremias Toledo

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -18,7 +18,8 @@ export const ItemDetailContainer = () => {
 
     }, [id]);
 
-    if(!product) return <div className='loading'>Loading product...</div>;
+    // if(!product) return <div className='loading'>Loading product...</div>;
+    if(!product) return <div className="spinner"></div>
   
     return (
     <ItemDetail product={product} />
